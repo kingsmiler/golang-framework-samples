3332dacd95eb4361995ea3c111b671226f8a0486
kingsmiler <kingsmiler@163.com>
2016-01-21 19:20:52 +0800

    add sirupsen log samples

---
+++ sirupsen-logrus/src/simplelog/simplelog.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+func main() {
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Info("A walrus appears")
+
+	log.Info("Hello, World!")
+}
